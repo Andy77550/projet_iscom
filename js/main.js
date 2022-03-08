25879d8772bf726da39fd32f17a9f2fcd4d21364
Andy77550 <74592653+Andy77550@users.noreply.github.com>
2022-03-08 17:07:27 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,23 +1,121 @@
-$(document).ready(function(){
-  $('.customer-logos').slick({
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1500,
-      arrows: false,
-      dots: false,
-      pauseOnHover: false,
-      responsive: [{
-          breakpoint: 768,
-          settings: {
-              slidesToShow: 4
-          }
-      }, {
-          breakpoint: 520,
-          settings: {
-              slidesToShow: 3
-          }
-      }]
-  });
-});
+/*==================== SWIPER DISCOVER ====================*/
+// let swiper = new Swiper(".discover__container", {
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     loop: true,
+//     spaceBetween: 32,
+//     coverflowEffect: {
+//         rotate: 0,
+//     },
+//  })
 
+let swiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+        rotate: 0,
+    },
+});
+ 
+ /*==================== VIDEO ====================*/
+const videoFile = document.getElementById('video-file'),
+videoButton = document.getElementById('video-button'),
+videoIcon = document.getElementById('video-icon')
+
+function playPause(){ 
+if (videoFile.paused){
+  // Play video
+  videoFile.play()
+  // We change the icon
+  videoIcon.classList.add('ri-pause-line')
+  videoIcon.classList.remove('ri-play-line')
+}
+else {
+  // Pause video
+  videoFile.pause(); 
+  // We change the icon
+  videoIcon.classList.remove('ri-pause-line')
+  videoIcon.classList.add('ri-play-line')
+
+}
+}
+videoButton.addEventListener('click', playPause)
+
+function finalVideo(){
+// Video ends, icon change
+videoIcon.classList.remove('ri-pause-line')
+videoIcon.classList.add('ri-play-line')
+}
+// ended, when the video ends
+videoFile.addEventListener('ended', finalVideo)
+
+ 
+ /*==================== SHOW SCROLL UP ====================*/ 
+ function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+ }
+ window.addEventListener('scroll', scrollUp);
+ 
+ 
+ 
+ 
+ /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+ const sections = document.querySelectorAll('section[id]')
+ 
+ function scrollActive(){
+     const scrollY = window.pageYOffset
+ 
+     sections.forEach(current =>{
+         const sectionHeight = current.offsetHeight
+         const sectionTop = current.offsetTop - 50;
+         sectionId = current.getAttribute('id')
+ 
+         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+         }else{
+             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+         }
+     })
+ }
+ window.addEventListener('scroll', scrollActive);
+ 
+ /*==================== SCROLL REVEAL ANIMATION ====================*/
+ const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2800,
+    // reset: true,
+ })
+ 
+ 
+ sr.reveal(`.home__data, .home__social-link, .info,
+           .discover__container,
+           .experience__data, .experience__overlay,
+           .place__card,
+           .sponsor__content,
+           .footer__data, .footer__rights`,{
+    origin: 'top',
+    interval: 100,
+ })
+ 
+ sr.reveal(`.about__data, 
+           .video__description,
+           .subscribe__description`,{
+    origin: 'left',
+ })
+ 
+ sr.reveal(`.about__img-overlay, 
+           .video__content,
+           .subscribe__form`,{
+    origin: 'right',
+    interval: 100,
+ })
+ 
+ 
